8c07014f86033d99675a62276573886d43b4844b
Christopher Bussen <cbussen7@gmail.com>
2021-06-22 12:53:17 -0500

    Integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,35 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+Blog.create!(
+title: "My Blog Post #{blog}",
+body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa commodi a sit libero, expedita velit, minima nemo
+esse voluptates unde, cumque et inventore explicabo. Explicabo debitis alias nihil suscipit voluptate!"
+)
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+Skill.create!(
+title: "Rails #{skill}",
+percent_utilized: 15
+)
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+Portfolio.create!(
+title: "Portfolio Title: #{portfolio_item}",
+subtitle: "My great service",
+body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime repellendus odit voluptate fugit laudantium
+facilis molestias corporis ullam quis sit, eos dignissimos?",
+thumb_image: "https://place-hold.it/350x200",
+main_image: "https://place-hold.it/600x400",
+)
+end
+puts "9 portfolio items created"
